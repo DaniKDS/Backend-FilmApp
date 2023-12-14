2d844167b2244dc45827459ec8aeafdccdb77864
GeaninaC26 <91499649+GeaninaC26@users.noreply.github.com>
2023-12-14 18:26:56 +0200

    add filter function for users

--- src/main/java/com/example/moviematchbackend/controller/UtilizatorController.java
+++ src/main/java/com/example/moviematchbackend/controller/UtilizatorController.java
@@ -1,6 +1,7 @@
 package com.example.moviematchbackend.controller;
 
 
+
 import com.example.moviematchbackend.models.dto.UtilizatorDto;
 import com.example.moviematchbackend.models.entity.Utilizator;
 import com.example.moviematchbackend.models.mapper.UtilizatorMapper;
@@ -12,6 +13,10 @@ import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 @RestController
 public class UtilizatorController {
 
@@ -49,6 +54,34 @@ public class UtilizatorController {
        String mail = ((DefaultOidcUser) authentication.getPrincipal()).getEmail();
        Utilizator user = utilizatorService.getUtilizatorByEmail(mail);
        return utilizatorMapper.utilizatorToUtilizatorDto(user);
+    }
+    @GetMapping("/api/filter_user/{searchText}")
+    public List<UtilizatorDto> handleFilter(@PathVariable String searchText) {
+        List<UtilizatorDto> user = getUtilizatori();
+        List<UtilizatorDto> user1 = getUtilizatori();
+
+        // Collect lowercased concatenated names into a set
+        Set<String> lowerCaseNames = user1.stream()
+                .map(utilizator -> utilizator.getNumeUtilizator().toLowerCase() + utilizator.getPrenumeUtilizator().toLowerCase())
+                .collect(Collectors.toSet());
+
+        // Filter users based on lowercase concatenated names present in lowerCaseNames set
+        Predicate<UtilizatorDto> nameMatchesSearchText = utilizator -> {
+            String fullNameLower = utilizator.getNumeUtilizator().toLowerCase() + utilizator.getPrenumeUtilizator().toLowerCase();
+            String searchTextLower = searchText.toLowerCase();
+            return fullNameLower.contains(searchTextLower);
+        };
+
+        // Apply the filter and collect the matching users
+        List<UtilizatorDto> filteredUsers = user.stream()
+                .filter(nameMatchesSearchText)
+                .collect(Collectors.toList());
+
+        return filteredUsers;
+    }
+    @GetMapping("/api/filter_user/")
+    public List<UtilizatorDto> emptyfilter(){
+        return getUtilizatori();
     }
 
 }
